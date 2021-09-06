39d8092a9d31715f0222942d83144aecf7f75e5e
Lucas Lisboa Coletti <llcoletti@universo.univates.br>
2021-09-06 18:56:50 -0300

    Adicionado relação com API para os demais tipos de vehículos

--- JavaScript/api.js
+++ JavaScript/api.js
@@ -1,17 +1,56 @@
+/*cars*/
+
 fetch('https://parallelum.com.br/fipe/api/v1/carros/marcas')
   .then(res =>res.json())
   .then(jsonResponse =>{
     /* console.log(jsonResponse) */
     
-    const data = jsonResponse.map(modelo =>{  
+    const dataCars = jsonResponse.map(modelo =>{  
         
         
         return `<li>${modelo.nome}</li>` // modelo.nome modelo.codigo
 
       })
       
-      const carModel = data.join(" ")
+      const carModel = dataCars.join(" ")
       document.querySelector("#carsList").insertAdjacentHTML("afterbegin", carModel)
+    
+  })
+
+  
+/*motorcycles*/
+fetch('https://parallelum.com.br/fipe/api/v1/motos/marcas')
+.then(res =>res.json())
+.then(jsonResponse =>{
+ 
+  
+  const dataMotorcyles = jsonResponse.map(modelo =>{  
+      
+      
+      return `<li>${modelo.nome}</li>` // modelo.nome modelo.codigo
+
+    })
+    
+    const motorcycleModel = dataMotorcyles.join(" ")
+    document.querySelector("#motorcyclesList").insertAdjacentHTML("afterbegin", motorcycleModel)
+  
+})
+
+
+/*trucks*/
+fetch('https://parallelum.com.br/fipe/api/v1/caminhoes/marcas')
+  .then(res =>res.json())
+  .then(jsonResponse =>{    
+    
+    const dataTrucks = jsonResponse.map(modelo =>{  
+        
+        
+        return `<li>${modelo.nome}</li>` // modelo.nome modelo.codigo
+
+      })
+      
+      const truckModel = dataTrucks.join(" ")
+      document.querySelector("#trucksList").insertAdjacentHTML("afterbegin", truckModel)
     
   })
 
