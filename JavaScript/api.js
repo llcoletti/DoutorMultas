6dab8dcfd467e6cd8bfbf6b941559c50be4d602e
Lucas Lisboa Coletti <llcoletti@universo.univates.br>
2021-09-08 20:26:01 -0300

    Configurado retorno da API para todos os típos de vehículo

--- JavaScript/api.js
+++ JavaScript/api.js
@@ -1,5 +1,6 @@
-/*cars*/
 
+
+//CARROS//
 fetch('https://parallelum.com.br/fipe/api/v1/carros/marcas')
   .then(res =>res.json())
   .then(jsonResponse =>{
@@ -49,41 +50,108 @@ fetch('https://parallelum.com.br/fipe/api/v1/carros/marcas')
   }
 )
   
-  
-/*motorcycles*/
+//MOTOS//
 fetch('https://parallelum.com.br/fipe/api/v1/motos/marcas')
   .then(res =>res.json())
   .then(jsonResponse =>{
-  
-  
-  const dataMotorcyles = jsonResponse.map(modelo =>{  
-      
-      
-      return `<li>${modelo.nome}</li>` // modelo.nome modelo.codigo
-
-  })
+    const dataMotorcycles = jsonResponse.map(fabricante =>{              
+      return `<li>${fabricante.nome} <p>${fabricante.codigo}</p></li> `
+    })    
+    //remove as "," do array e insere os elementos no HTML    
+    const MotorcyclesModel = dataMotorcycles.join(" ")
     
-    const motorcycleModel = dataMotorcyles.join(" ")
-    document.querySelector("#motorcyclesList").insertAdjacentHTML("afterbegin", motorcycleModel)
-  
-})
+    document.querySelector("#motorcyclesList").insertAdjacentHTML("afterbegin", MotorcyclesModel) 
+    console.log(MotorcyclesModel)
+    const fabricSelect = document.querySelectorAll('#menu .menu_motorcycles .grid li')     
+      
+      //Define event listener para os elementos adicionados ao HTML
+    for (const element of fabricSelect){
+      //altera para o menu secundário
+      element.addEventListener('click', function() {
+        footer.classList.add('hidden')
+        secondaryMenu.classList.toggle('show')
+        mainMenu.classList.remove('show')
+        returnButton.classList.add('show')                        
+        //atribui o elemento clicado ao fabricante.          
+        //regEx remove elementos html concatenado com a tag desejada       
+        document.querySelector("#fabricante").insertAdjacentHTML("afterbegin",`<h2>${element.outerHTML.replace(/<(?:.|\n)*?>/gm, '').replace(/\d+/g, '')}</h2>`)
+        //atribui código do vehículo a uma constante
+        const motorcycleCod = element.outerHTML.replace(/\D/g, "")
+        //Submenu
+        fetch(`https://parallelum.com.br/fipe/api/v1/motos/marcas/${motorcycleCod}/modelos`)
+          .then(res =>res.json())
+          .then(jsonResponse =>{       
+            console.log(jsonResponse.modelos)
+            const modeloMotorcycle = jsonResponse.modelos.map(modelo =>{
+              return `<li>${modelo.nome}</li>`
+            })
+            //Coloca os modelos em ordem decrescente
+            ordenaModelo = modeloMotorcycle.sort((a, b)=>{
+              if(a>b)
+              return -1;
+              if(a<b){
+                return 1;                  
+              }
+              return 0;
+            })
+            document.querySelector("#modelList").insertAdjacentHTML("afterbegin", ordenaModelo.join(" "))
+          })
+        }
+      )
+    }
+  }
+)
 
 
-/*trucks*/
+//CAMINHOES//
 fetch('https://parallelum.com.br/fipe/api/v1/caminhoes/marcas')
   .then(res =>res.json())
-  .then(jsonResponse =>{    
+  .then(jsonResponse =>{
+    const dataTrucks = jsonResponse.map(fabricante =>{              
+      return `<li>${fabricante.nome} <p>${fabricante.codigo}</p></li> `
+    })    
+    //remove as "," do array e insere os elementos no HTML    
+    const trucksModel = dataTrucks.join(" ")
     
-    const dataTrucks = jsonResponse.map(modelo =>{          
-        
-      return `<li>${modelo.nome}</li>` // modelo.nome modelo.codigo
-
-      })
-    
-    const truckModel = dataTrucks.join(" ")
-    document.querySelector("#trucksList").insertAdjacentHTML("afterbegin", truckModel)
-    
-  })
-
-
+    document.querySelector("#trucksList").insertAdjacentHTML("afterbegin", trucksModel) 
+    console.log(trucksModel)
+    const fabricSelect = document.querySelectorAll('#menu .menu_trucks .grid li')     
+      
+      //Define event listener para os elementos adicionados ao HTML
+    for (const element of fabricSelect){
+      //altera para o menu secundário
+      element.addEventListener('click', function() {
+        footer.classList.add('hidden')
+        secondaryMenu.classList.toggle('show')
+        mainMenu.classList.remove('show')
+        returnButton.classList.add('show')                        
+        //atribui o elemento clicado ao fabricante.          
+        //regEx remove elementos html concatenado com a tag desejada       
+        document.querySelector("#fabricante").insertAdjacentHTML("afterbegin",`<h2>${element.outerHTML.replace(/<(?:.|\n)*?>/gm, '').replace(/\d+/g, '')}</h2>`)
+        //atribui código do vehículo a uma constante
+        const motorcycleCod = element.outerHTML.replace(/\D/g, "")
+        //Submenu
+        fetch(`https://parallelum.com.br/fipe/api/v1/caminhoes/marcas/${motorcycleCod}/modelos`)
+          .then(res =>res.json())
+          .then(jsonResponse =>{       
+            console.log(jsonResponse.modelos)
+            const modeloTrucks = jsonResponse.modelos.map(modelo =>{
+              return `<li>${modelo.nome}</li>`
+            })
+            //Coloca os modelos em ordem decrescente
+            ordenaModelo = modeloTrucks.sort((a, b)=>{
+              if(a>b)
+              return -1;
+              if(a<b){
+                return 1;                  
+              }
+              return 0;
+            })
+            document.querySelector("#modelList").insertAdjacentHTML("afterbegin", ordenaModelo.join(" "))
+          })
+        }
+      )
+    }
+  }
+)
  
